1c1ebd1e31e4dba4b3a9b1342eef2dca19e4f200
torone89 <torone89>
2022-06-21 15:47:22 +0200

    Quando devo applicare lo sconto?

--- js/script.js
+++ js/script.js
@@ -10,15 +10,16 @@ console.log('JS OK');
 3 Calcolo il totale del viaggio senza sconto
 4 Stampo il totale del viaggio senza sconto e inserisco html
 5 Calcolo Sconti del 20 % e del 60%
+6 Quando devo applicare lo sconto?
 
 */
 
 // FASE 1 e 2
 
-const età = prompt("Quanti anni hai?", "10");
+const età = parseInt(prompt("Quanti anni hai?", "10"));
 console.log(età);
 
-const kilometri = prompt("Quanti kilometri vuoi percorrere", "20");
+const kilometri = parseInt(prompt("Quanti kilometri vuoi percorrere", "20"));
 console.log(kilometri);
 
 // FASE 3
@@ -45,3 +46,16 @@ console.log(saldoMinori)
 
 const saldoAnziani = totaleViaggio - (totaleViaggio * 0.4)
 console.log(saldoAnziani)
+
+// Fase 6
+
+if (età < 18) { console.log("Hai uno sconto del " + saldoMinori) }
+
+else if (età > 65) {
+    console.log("Hai uno sconto del 20%" + saldoAnziani)
+
+}
+else {
+    console.timeLog("non hai sconti")
+
+}
